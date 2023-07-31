9b5add5645c40e243a633428abdd63eb5ee371d2
Peter Hoenig <hpfreak1004@gmail.com>
2023-07-31 08:38:10 +0200

    add itodd support

---
+++ train_itodd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Assuming you have a bash script named 'train_pix2pose.sh'
+
+for i in {15..30}; do
+  python3 tools/3_train_pix2pose.py 0 cfg/cfg_bop2020_rgb_custom.json itodd $i pix2pose_datasets/coco2017/train2017
+done
--- train_tless_random.sh
+++ train_tless_random.sh
@@ -2,6 +2,6 @@
 
 # Assuming you have a bash script named 'train_pix2pose.sh'
 
-for i in {1..30}; do
+for i in {15..30}; do
   python3 tools/3_train_pix2pose.py 0 cfg/cfg_bop2020_rgb_custom.json tless_random_texture $i pix2pose_datasets/coco2017/train2017
 done
